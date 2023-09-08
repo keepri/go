794bbd710449a219acf6cb1402c6c31714a8e0ab
KIPRI <hi@kipri.dev>
2023-09-08 15:49:27 +0300

    moved auth routes inside of handler

--- internal/api/routes.go
+++ internal/api/routes.go
@@ -8,16 +8,14 @@ import (
 )
 
 func ApiRoute(r chi.Router) {
-	r.Route("/auth", authRoute)
+	r.Route("/auth", func(r chi.Router) {
+		r.Post("/login", handlers.Login)
+		r.Post("/logout", handlers.Logout)
+		r.HandleFunc("/", func(w http.ResponseWriter, _ *http.Request) {
+			w.Write([]byte("Hello, auth!"))
+		})
+	})
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello, api!"))
-	})
-}
-
-func authRoute(r chi.Router) {
-	r.Post("/login", handlers.Login)
-	r.Post("/logout", handlers.Logout)
-	r.HandleFunc("/", func(w http.ResponseWriter, _ *http.Request) {
-		w.Write([]byte("Hello, auth!"))
 	})
 }
